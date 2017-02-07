9837624a65ccb88a3293a67e8c8760e570b07bb0
Ollin Demian <ollin.langle@ciencias.unam.mx>
2017-02-06 23:23:58 -0600

    Se usa la bandera -a para tee en el ejecutable y agregamos el reporte

--- tarea_1/ejecutable.sh
+++ tarea_1/ejecutable.sh
@@ -6,7 +6,7 @@ d=; n=0; until [ "$d" = "20161201" ]; do ((n++)); d=$(date -d "today - $n days" 
 
 zip 00000000.export.CSV.zip headers.csv && mv 00000000.export.CSV.zip ./archivos
 
-parallel gunzip -c ::: $(ls ./archivos/*.export.CSV.zip) | awk '($2=="SQLDATE" || $8=="MEX" || $18=="MEX")  {print}' | tee salida.txt | csvsql --db sqlite:///gdelt.db --table mexico --insert -t 
+parallel gunzip -c ::: $(ls ./archivos/*.export.CSV.zip) | awk '($2=="SQLDATE" || $8=="MEX" || $18=="MEX")  {print}' | tee -a salida.txt | csvsql --db sqlite:///gdelt.db --table mexico --insert -t 
 
 awk -F "\t" '($57!="DATEADDED"){a[$57]+=$31;b[$57]+=1}END{for(i in a) print i "\t" b[i] "\t" a[i]/b[i]}' salida.txt >> gs.txt && sed -i "1iDate \t NumberOfOcurrences \t GoldsteinScale" gs.txt && cat gs.txt |  csvsql --db sqlite:///gdelt.db --table mexico_ts --insert -t
 
